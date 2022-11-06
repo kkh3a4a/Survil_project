96b6cd37ac928ed3f410702e4a7b83c25669f50d
WooYung <83138945+TPyun@users.noreply.github.com>
2022-11-06 19:13:57 +0900

    Server fixed

--- Project/Source/Project/Private/Server_testing.cpp
+++ Project/Source/Project/Private/Server_testing.cpp
@@ -27,7 +27,7 @@ void AServer_testing::BeginPlay()
 
 	//connect();
 	ret = connect(s_socket, reinterpret_cast<sockaddr*> (&server_addr), sizeof(server_addr));
-	
+	UE_LOG(LogTemp, Log, TEXT("Connected   ==========================================="));
 }
 
 // Called every frame
@@ -35,27 +35,27 @@ void AServer_testing::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	char buf[256];
-	cout << "Enter : ";
-	cin.getline(buf, BUFSIZE);
-	DWORD sent_byte;
-	WSABUF mybuf;
-	mybuf.buf = buf;
-	mybuf.len = strlen(buf) + 1;
-	ret = WSASend(s_socket, &mybuf, 1, &sent_byte, 0, 0, 0);
-	if (SOCKET_ERROR == ret)
-	{
-		int err_num = WSAGetLastError();
-	}
+	//char buf[256];
+	////cout << "Enter : ";
+	////cin.getline(buf, BUFSIZE);
+	//DWORD sent_byte;
+	//WSABUF mybuf;
+	//mybuf.buf = buf;
+	//mybuf.len = strlen(buf) + 1;
+	//ret = WSASend(s_socket, &mybuf, 1, &sent_byte, 0, 0, 0);
+	//if (SOCKET_ERROR == ret)
+	//{
+	//	int err_num = WSAGetLastError();
+	//}
 
-	char recv_buf[BUFSIZE];
-	WSABUF mybuf_r;
-	mybuf_r.buf = recv_buf;
-	mybuf_r.len = BUFSIZE;
-	DWORD recv_byte;
-	DWORD recv_flag = 0;
-	WSARecv(s_socket, &mybuf_r, 1, &recv_byte, &recv_flag, 0, 0);
-	cout << "Server Sent [" << recv_byte << "bytes] : " << recv_buf << endl;
+	//char recv_buf[256];
+	//WSABUF mybuf_r;
+	//mybuf_r.buf = recv_buf;
+	//mybuf_r.len = 256;
+	//DWORD recv_byte;
+	//DWORD recv_flag = 0;
+	//WSARecv(s_socket, &mybuf_r, 1, &recv_byte, &recv_flag, 0, 0);
+	//cout << "Server Sent [" << recv_byte << "bytes] : " << recv_buf << endl;
 
 }
 
