86967e838ea230d0d8fd0c4e70dd1f90ed3688b4
kjhgme <yerw5570@gmail.com>
2023-02-27 13:35:56 +0900

    적업일지

--- Server/terrain.cu
+++ Server/terrain.cu
@@ -18,12 +18,12 @@ typedef struct two_int {
 	int y;
 } II;
 
-//const int one_side_number = 320;	//32000
+const int one_side_number = 320;	//32000
 //const int player_sight_size = 50;	//1024 ³ÑÀ¸¸é ¾ÈµÊ
-//const int random_array_size = 500000;// 90000000;
-const int one_side_number = 16000;	//32000
+const int random_array_size = 500000;// 90000000;
+//const int one_side_number = 16000;	//32000
 II player_sight_size{200, 120};	//1024 ³ÑÀ¸¸é ¾ÈµÊ
-const int random_array_size = 50000000;// 90000000;
+//const int random_array_size = 50000000;// 90000000;
 //const int one_side_number = 32000;	//32000
 //const int player_sight_size = 200;	//1024 ³ÑÀ¸¸é ¾ÈµÊ
 //const int random_array_size = 90000000;// 90000000;
