bac70fd5fccf36a9b9e48a028f16af2d7b6c4062
zzapuno <zzapuno@hotmail.com>
2023-05-11 19:30:06 +0900

    2023-05-11 종합설계 1

--- Project/Source/Project/Public/NetworkingThread.h
+++ Project/Source/Project/Public/NetworkingThread.h
@@ -71,8 +71,8 @@ public:
 
 private: 
 	//char IPAddress[20] = "192.168.0.8";
-	//char IPAddress[20] = "127.0.0.1";
-	char IPAddress[20] = "172.20.10.2";
+	char IPAddress[20] = "127.0.0.1";
+	//char IPAddress[20] = "172.20.10.2";
     int32 BytesReceived;
     int32 BytesSent;
 
