02f3d38de7db9c6556a183bc19e5048151011626
kjhgme <kjhgme@naver.com>
2023-07-23 04:04:05 +0900

    Anim추가, Army 클릭 잘되게 수정

--- IOCPServer/Citizen.h
+++ IOCPServer/Citizen.h
@@ -13,7 +13,7 @@ public:
 	float _job_x, _job_y, _job_z;
 	int _Job_id;
 	int _house_id;	// -1 : 집없음, 나머지 : 집 object id
-	char _citizenstate;		// 0 : idle, 1 : wark, 2 : sleep
+	char _citizenstate;		// 0 : idle, 1 : walk, 2 : sleep
 	
 	char _satiety;	//0~100
 	char _thirsty;	//0~100
