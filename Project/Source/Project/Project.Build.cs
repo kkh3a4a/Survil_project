1f78ec0ed02100d797d41cfd33853970d0ea4c44
kkh3a4a <kkh3a4a@naver.com>
2023-07-30 19:25:56 +0900

    Update Project.Build.cs

--- Project/Source/Project/Project.Build.cs
+++ Project/Source/Project/Project.Build.cs
@@ -12,7 +12,7 @@ public class Project : ModuleRules
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Landscape", "Networking", "ProceduralMeshComponent", "Sockets" });
         PublicDependencyModuleNames.AddRange(new string[] { });
         PrivateDependencyModuleNames.AddRange(new string[] { });
-
+        bEnableExceptions = true;
         // Uncomment if you are using Slate UI
         // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
